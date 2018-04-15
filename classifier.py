52364a771832e107f1d0698de1367c88395f1510
Prerit Garg <prerit2010@gmail.com>
2018-04-16 01:58:19 +0530

    dependents checked in rightArc()

--- classifier.py
+++ classifier.py
@@ -28,13 +28,22 @@ def leftArc(s1, s2):
         return False
 
 
-def rightArc(s1, s2):
+def rightArc(s1, s2, index_i, index_j):
     
     m, n, p, q = s1[1], s1[2], s2[1], s2[2]
+    s1, s2 = train_data[m][m], train_data[p][q]
     # need to check all dependents too
-    if train_data[m][n]['head'] == 0:
+
+    for i, word_data in enumerate(train_data[index_i]):
+        w1 = (word_data['form'], index_i, i)
+        w2 = (s1['form'], index_i, index_j)
+        if word_data['head'] == s1['id']:
+            if ((w1, w2) or (w2, w1)) not in relations
+                return False
+
+    if s1['head'] == 0:
         return True
-    if train_data[m][n]['head'] == train_data[p][q]['id']:
+    if s1['head'] == s2['id']:
         return True
     else
         return False
@@ -51,14 +60,13 @@ for i, data in enumerate(train_data):
             # iterate over the stack contents
             while(len(stack) > 1):
                 # store top 2 elements of stack in s1 and s2
-                s1 = stack[-1]
-                s2 = stack[-2]
+                s1, s2 = stack[-1], stack[-2]
 
                 if leftArc(s1, s2):
                     relations.append((s2, s1))
                     stack.pop(-2)
                     operation_labels.append(0)
-                elif rightArc(s1, s2):
+                elif rightArc(s1, s2, i, j):
                     relations.append((s1, s2))
                     stack.pop()
                     operation_labels.append(1)
